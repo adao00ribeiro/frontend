ffbc9e0898431288ad4ca86f5370eebaf546b42b
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-01-23 23:59:22 -0300

    Add Interface ICourse

--- interfaces/ICourse.ts
+++ interfaces/ICourse.ts
@@ -3,7 +3,7 @@
 export interface ICourse {
     id?: string;
     name: string;
-    description: string;
+    description?: string;
     modality: string;
     type: string;
     investmentvalue: number;
